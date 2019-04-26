1de515c83e3c16b3bab87c8019da86a7801e3982
grumpypenny <ajitesh.misra.14@gmail.com>
2019-04-26 16:38:33 -0400

    Fixed default AI
    Added way to move 2D objects

---
+++ Ludum Dare 44/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAI : MonoBehaviour
+{
+	public NavMeshAgent agent;
+
+	public Camera cam;
+
+	public float difference = 2.0f;
+	public float speed;
+
+	/*
+	 * This script goes on the child of an agent
+	 */
+
+
+	void Awake()
+    {
+		cam = Camera.main;
+		if (speed == 0.0)
+		{
+			speed = agent.speed;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// reset transform to normal
+		transform.eulerAngles = new Vector3(0, 0, 0);
+		Move();
+    }
+
+	private void Move()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
