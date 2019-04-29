979647fba8de1f9d03d6b4e6e4c907d99d91eb2e
grumpypenny <ajitesh.misra.14@gmail.com>
2019-04-28 21:00:51 -0400

    Final for day 2?

---
+++ Ludum Dare 44/Assets/Scripts/Graveyard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard : MonoBehaviour
+{
+	public GameObject blood;
+
+	public float timeBetweenOfferings = 10f;
+
+	private float timeOfNextOffering = 21;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Time.time > timeOfNextOffering)
+		{
+			Instantiate(blood, transform.position, Quaternion.identity);
+			timeOfNextOffering = Time.time + timeBetweenOfferings;
+		}
+    }
+}
